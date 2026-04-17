acd21454d16b05161d7451164386230bf7b1350d
Brylle <brylledejesus272gmail.com>
2026-04-17 23:01:14 +0800

    Remove duplicate admin insert from seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,5 +1,3 @@
-INSERT INTO users (username, password) VALUES
-('admin', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
 
 INSERT INTO categories (name, slug) VALUES
 ('Tutorial', 'tutorial'),
